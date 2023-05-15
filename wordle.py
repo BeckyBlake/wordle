6b4b639821b4300ef3a170f2039e0cec16ca046c
Becky Blake <ryblake2@illinois.edu>
2023-05-15 18:30:12 -0500

    create stats graph

--- wordle.py
+++ wordle.py
@@ -1,6 +1,12 @@
 import pygame
 import random
+import matplotlib
+matplotlib.use('module://pygame_matplotlib.backend_pygame')
+import matplotlib.pyplot as plt
+import numpy as np
 pygame.init()
+
+
 
 clock = pygame.time.Clock()
 
@@ -20,6 +26,7 @@ black = (0, 0, 0)
 gray = (200, 200, 200)
 green = (50, 168, 82)
 yellow = (207, 209, 105)
+red = (255, 0, 0)
 
 answer = "xxxxx"
 
@@ -28,16 +35,24 @@ win_type = [0, 0, 0, 0, 0, 0]
 played = 0
 max_streak = 0
 current_streak = 0
+exited_from_play_again = 0
+
+
+stat_img = pygame.image.load("stats.png").convert()
+stat_img = pygame.transform.scale(stat_img, (50, 50))
+
 
 
 def set_answer():
     # print("hello")
-    global answer
+    global answer, exited_from_play_again
     # file = open('words.txt')
     # content = file.readlines()
     # line_number = random.randint(0, len(content))
     # answer = content[line_number].strip()
     answer = "later"
+    # we know for a fact a new game has started so
+    exited_from_play_again = 0
 
 
 # set up boxes
@@ -96,6 +111,8 @@ def set_up_colored_boxes():
 
 
 def display_user_input():
+    if exited_from_play_again == 1:
+        return
     y = current_word*70 + 800/4 + 10
     font = pygame.font.Font(None, 50)
     for i in range(0, len(user_input)):
@@ -112,15 +129,101 @@ def display_guessed_words():
 
 user_input = ""
 
-exited_from_play_again = 0
-
 def play_again_request():
     global user_input, current_word, played, exited_from_play_again
     played += 1
+    display_stats(1)
+    print(guessed_words)
+    print("returning from play again")
+    # rectangle = pygame.Rect(width/2, height/2, 350, 400)
+    # rectangle.center = (width/2, height/2)
+    # inner_rectangle = pygame.Rect(width/2, height/2, 280, 100)
+    # inner_rectangle.center = (width/2, height/2 + 120)
+
+
+    # while True:
+    #     for event in pygame.event.get():
+    #         if event.type == pygame.QUIT:
+    #             pygame.quit()
+    #         if event.type == pygame.MOUSEBUTTONDOWN:
+    #             mouse_pos = event.pos
+    #             if inner_rectangle.collidepoint(mouse_pos):
+    #                 user_input = ""
+    #                 current_word = 0
+    #                 guessed_words.clear()
+    #                 set_answer()
+    #                 return
+    #             elif exit_rect.collidepoint(mouse_pos):
+    #                 exited_from_play_again = 1
+    #                 return
+       
+    #     pygame.draw.rect(screen, gray, rectangle)
+    #     pygame.draw.rect(screen, white, inner_rectangle)
+    #     font = pygame.font.Font(None, 50)
+    #     text = font.render("Play again?", True, black)
+    #     text_rect = text.get_rect(center=(width/2, height/2 + 120))
+    #     exit = font.render("x", True, black)
+    #     exit_rect = exit.get_rect(center=(width/2 + 150, height/2 - 180))
+    #     screen.blit(exit, exit_rect)
+    #     screen.blit(text, text_rect)
+    #     pygame.display.flip()
+
+def create_graph():
+    plt.rcdefaults()
+    fig, ax = plt.subplots()
+
+    number_labels = ('1', '2', '3', '4', '5', '6')
+    bars = ax.barh(number_labels, win_type, align='center', color='gray')
+
+    # Set a small height for bars with a value of 0
+    small_height = max(max(win_type) * 0.05, 0.05)
+
+    # Add the values as text annotations inside the bars
+    for i, bar in enumerate(bars):
+        value = win_type[i]
+        if value == 0:
+            bar.set_width(small_height)
+        # ax.text(value + 0.01, bar.get_y() + bar.get_height() / 2, str(value),
+        #         ha='left', va='center', fontsize=11, color='red', fontweight='bold')
+        ax.annotate(str(value), xy=(bar.get_width() / 2 - 0.015, bar.get_y() + bar.get_height() / 2),
+                xytext=(3, 0), textcoords='offset points', ha='left', va='center', fontsize=11, color='white')
+
+        label = number_labels[i]
+        ax.text(0, bar.get_y() + bar.get_height() / 2, label, ha='right', va='center', fontsize=11)
+
+    ax.spines['left'].set_visible(False)
+    ax.spines['right'].set_visible(False)
+    ax.spines['top'].set_visible(False)
+    ax.spines['bottom'].set_visible(False)
+
+
+
+    plt.axis('off')
+    # Remove the y-axis ticks and labels
+    ax.yaxis.set_ticks([])
+    ax.set_yticklabels([])
+
+
+    fig.canvas.draw()
+    fig = pygame.transform.scale(fig, (640*3/4, 480*3/4))
+    return fig
+
+def display_stats(play_again_flag):
+    global exited_from_play_again, wins, played, max_streak, current_streak, win_type, screen, user_input, current_word, guessed_words
     rectangle = pygame.Rect(width/2, height/2, 350, 400)
     rectangle.center = (width/2, height/2)
+
+    win_percent = 0
+    if played == 0:
+        win_percent = 0
+    else:
+        win_percent = wins/played * 100
+
     inner_rectangle = pygame.Rect(width/2, height/2, 280, 100)
-    inner_rectangle.center = (width/2, height/2 + 120)
+    inner_rectangle.center = (width/2, height * 4/5)
+
+    fig = create_graph()
+    
 
     while True:
         for event in pygame.event.get():
@@ -129,26 +232,32 @@ def play_again_request():
             if event.type == pygame.MOUSEBUTTONDOWN:
                 mouse_pos = event.pos
                 if inner_rectangle.collidepoint(mouse_pos):
+                    exited_from_play_again = 0
                     user_input = ""
                     current_word = 0
                     guessed_words.clear()
                     set_answer()
                     return
                 elif exit_rect.collidepoint(mouse_pos):
-                    exited_from_play_again = 1
+                    if play_again_flag == 1:
+                        exited_from_play_again = 1
                     return
-       
+                elif not rectangle.collidepoint(mouse_pos):
+                    if play_again_flag == 1:
+                        exited_from_play_again = 1
+                    return
+        
         pygame.draw.rect(screen, gray, rectangle)
-        pygame.draw.rect(screen, white, inner_rectangle)
-        font = pygame.font.Font(None, 50)
-        text = font.render("Play again?", True, black)
-        text_rect = text.get_rect(center=(width/2, height/2 + 120))
         exit = font.render("x", True, black)
         exit_rect = exit.get_rect(center=(width/2 + 150, height/2 - 180))
+        graph_rect = fig.get_rect(center=(width/2, height/2 + 40))
         screen.blit(exit, exit_rect)
-        screen.blit(text, text_rect)
-        pygame.display.flip()
+        screen.blit(fig, graph_rect)
 
+        if play_again_flag == 1:
+            pygame.draw.rect(screen, red, inner_rectangle)
+        pygame.display.flip()
+        # pygame.display.update()
 
 set_answer()
 running = True
@@ -188,6 +297,12 @@ while running:
                 if len(user_input) != 5:
                     user_input += event.unicode
                     # print(user_input)
+        elif event.type == pygame.MOUSEBUTTONDOWN:
+            if stat_img_rect.collidepoint(event.pos):
+                if exited_from_play_again == 1:
+                    display_stats(1)
+                else:
+                    display_stats(0)
 
     # Clear the screen
     screen.fill(white)
@@ -205,6 +320,8 @@ while running:
     text = font.render("Wordle", True, black)
     text_rect = text.get_rect(center=(width/2, 50))
     screen.blit(text, text_rect)
+    stat_img_rect = stat_img.get_rect(center=(width*3/4, height/6))
+    screen.blit(stat_img, stat_img_rect)
 
         
     
